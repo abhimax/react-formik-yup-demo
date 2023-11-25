aff8d91c810ee5b8657736c1bdeea985a3ae96ca
Abhiman Ranaweera <abhiman.max@gmail.com>
2023-11-25 07:48:41 +0530

    feat: 2nd type formik forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import SignUpForm from "./components/SignUpForm";
+import SignUpFormType2 from "./components/SignUpFormType2";
 
 function App() {
   return (
     <div className="app-wrapper">
-      <h3>Formik and Yup Form Demo</h3>
+      <h2>Formik and Yup Form Demo</h2>
+      <h4>Method 01 : useFormik hook</h4>
       <SignUpForm />
+      <h4>Method 02 : Formik Form and Inputs</h4>
+      <SignUpFormType2 />
     </div>
   );
 }
---
+++ src/components/SignUpFormType2.jsx
@@ -0,0 +1,85 @@
+import { useFormik } from "formik";
+import "../styles/sign-up-form.css";
+import { useEffect } from "react";
+import { signUpValidation } from "./signUpValidation";
+const initialValues = {
+  name: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
+function SignUpFormType2() {
+  const { values, handleSubmit, handleBlur, handleChange, errors } = useFormik({
+    initialValues,
+    validationSchema: signUpValidation,
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+  useEffect(() => {
+    console.log(values);
+  }, [values]);
+
+  return (
+    <form className="sign-up-form" onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={values.name}
+          onChange={handleChange}
+          onBlur={handleBlur}
+        />
+        {errors.name && <small className="error-text">{errors.name}</small>}
+      </div>
+
+      <div className="form-group">
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={values.email}
+          onChange={handleChange}
+          onBlur={handleBlur}
+        />
+        {errors.email && <small className="error-text">{errors.email}</small>}
+      </div>
+
+      <div className="form-group">
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={values.password}
+          onChange={handleChange}
+          onBlur={handleBlur}
+        />
+        {errors.password && (
+          <small className="error-text">{errors.password}</small>
+        )}
+      </div>
+
+      <div className="form-group">
+        <label htmlFor="confirmPassword">Confirm Password:</label>
+        <input
+          type="password"
+          id="confirmPassword"
+          name="confirmPassword"
+          value={values.confirmPassword}
+          onChange={handleChange}
+          onBlur={handleBlur}
+        />
+        {errors.confirmPassword && (
+          <small className="error-text">{errors.confirmPassword}</small>
+        )}
+      </div>
+
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+export default SignUpFormType2;
